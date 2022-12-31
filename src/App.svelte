e657ec2162e2a0c5458217701749a6569e9f6648
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-12-31 17:40:39 -0500

    :tada: Begin

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  let randomNumber = Math.random();
+
+  setInterval(() => {
+    randomNumber = Math.random();
+  }, 1000);
+</script>
+
+<main class="h-screen flex flex-col text-center">
+  <h1 class="bg-yellow-300 text-5xl p-5">Random Number! 💪</h1>
+
+  <div class="flex-1 flex">
+    <p class="m-auto text-3xl">
+      {randomNumber}
+    </p>
+  </div>
+
+  <footer class="bg-yellow-200 text-3xl p-5">By Eliaz Bobadilla</footer>
+</main>
